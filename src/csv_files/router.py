f299c02f36367282eafc373a14267779586eac39
ShatAlex <Shaturnyyy2@gmail.com>
2023-06-05 10:39:39 +0300

    done

--- src/csv_files/router.py
+++ src/csv_files/router.py
@@ -15,6 +15,7 @@ router_files = APIRouter(
 @router_files.post('/upload')
 def upload_csv(name: str, bg_task: BackgroundTasks, file: UploadFile = File(...),
                csv_files_service: CsvFilesService = Depends()):
+    """ Загрузка csv файла """
     bg_task.add_task(csv_files_service.import_csv, name, file.file)
     return {"status": "success"}
 
@@ -23,6 +24,7 @@ def upload_csv(name: str, bg_task: BackgroundTasks, file: UploadFile = File(...)
 @cache(expire=600)
 async def review_csv(table_name: str, limit: int = 50, offset: int = 0, filter: Optional[str] = None,
                      order_by: Optional[str] = None, csv_files_service: CsvFilesService = Depends()):
+    """ Получение данных из конкретного файла с опциональной фильтрацией и сортировкой """
     data = await csv_files_service.get_csv(table_name, limit, offset, filter, order_by)
     return {
         'status': 'success',
@@ -34,6 +36,7 @@ async def review_csv(table_name: str, limit: int = 50, offset: int = 0, filter: 
 @router_files.get('/review')
 @cache(expire=600)
 async def review_csv(csv_files_service: CsvFilesService = Depends()):
+    """  Получение списка файлов с информацией о колонках """
     data = await csv_files_service.review_csv()
     return {
         'status': 'success',
@@ -44,5 +47,6 @@ async def review_csv(csv_files_service: CsvFilesService = Depends()):
 
 @router_files.delete('/{table_name}')
 async def delete_csv(table_name: str, csv_files_service: CsvFilesService = Depends()):
+    """ Удаление csv файла """
     await csv_files_service.drop_csv(table_name)
     return {"status": "success"}
--- src/main.py
+++ src/main.py
@@ -9,8 +9,22 @@ from auth.schemas import UserRead, UserCreate
 from csv_files.router import router_files
 from config import REDIS_HOST, REDIS_PORT
 
+tags_metadata = [
+    {
+        'name': 'Auth',
+        'description': 'Авторизация и регистрация',
+    },
+    {
+        'name': 'Files',
+        'description': 'Работа с csv файлами',
+    },
+]
+
 app = FastAPI(
-    title='CSV API'
+    title='CSV API',
+    description='Сервис для работы с ипортируемым csv файлами разной структуры',
+    version='1.0.0',
+    openapi_tags=tags_metadata
 )
 
 app.include_router(
